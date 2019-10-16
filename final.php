8c850d4aa273a4c539680c6c83709dfe395e57cb
Ty Hobbs <tjhobbs1@dmacc.edu>
2019-10-16 06:08:40 -0500

    Updated

---
+++ final.php
@@ -0,0 +1,11 @@
+
+<?php
+$base = $_POST["base"];
+$sending = $_POST["sending"];
+$ifrVfr = $_POST['ifrVfr'];
+$sceneHosp = $_POST['sceneHosp'];
+
+?>
+
+MercyOne AirMed <?php echo $base ?> was dispatched to <?php echo $sending." " .$ifrVfr. " " ?>
+for a <?php echo $sceneHosp ?>
